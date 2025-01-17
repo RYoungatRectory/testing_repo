ac1d0a0d221c29af315c062328c302436a369571
RYoungatRectory <remington.young@rectorywolves.org>
2025-01-17 08:31:40 -1000

    alph list

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-print("I love git")
+alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def cypher(N):
+	map = {}
+	for i in range(
+
+
+msg = input("Plaintext: ")
+shift = input("How much do you want to shift by: ")
+
+
 
