1a748bbcfc4d2cbb7f91e13a75c42e136ac4fce4
RYoungatRectory <remington.young@rectorywolves.org>
2025-01-22 06:10:42 -1000

    function + print statement added

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def keystream_gen(plaintext, key):
-	keysteam = ""
+	keystream = ""
 	j = 0
 	for i in range(len(plaintext)):
 		keystream += key[j]
@@ -15,5 +15,15 @@ if len(key) == 0:
 keyst = keystream_gen(plaintext, key)
 
 alph = "abcedfghijklmnopqrstuvwxyz"
-
+def viginere(plaintext, keyst):
+	encrypt = ""
+	for i in range(len(plaintext)):
+		charIndex = alph.index(plaintext[i])
+		keyIndex = alph.index(keyst[i])
+		x = charIndex + keyIndex
+		if x > len(alph)-1:
+			x -= len(alph)
+		encrypt += alph[x]
+	return encrypt
+print(viginere(plaintext, keyst))
 			
